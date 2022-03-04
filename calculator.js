cf9d674127bdbfafe01060aa8c60b7b4a42146fc
Michaël <cornelismichael@hotmail.com>
2022-03-04 14:21:31 +0100

    first basic functionality and draft layout

--- calculator.js
+++ calculator.js
@@ -1,10 +1,93 @@
+let firstNumber = '';
+let secondNumber = '';
+let currentOperator = '';
+
+//select all html elements
+const lastOperationScreen = document.getElementById('lastOperation');
+const currentOperationScreen = document.getElementById('currentOperation');
+const numberButtons = document.querySelectorAll('.button.number');
+const operatorButtons = document.querySelectorAll('.button.operator');
+const equalsButton = document.getElementById('=');
+const pointButton = document.getElementById('0');
+const clearButton = document.getElementById('clearButton');
+const deleteButton = document.getElementById('deleteButton');
+
+clearButton.addEventListener('click', clearCalculation);
+deleteButton.addEventListener('click', deleteLastNumber);
+numberButtons.forEach(button => button.addEventListener('click', updateCurrentOperation));
+operatorButtons.forEach(button => button.addEventListener('click', assignOperator));
+equalsButton.addEventListener('click', calculateFinalResult);
 
 
 
+function updateCurrentOperation(e) {
+    if (currentOperationScreen.textContent === '0') { 
+        removeZero();
+    }
+    currentOperationScreen.textContent += e.target.id;
+    console.log(e.target.id);
+}
 
+function assignOperator(e) {
+    if(lastOperationScreen.textContent !== '') {
+        secondNumber = currentOperationScreen.textContent;
+        let result = operate(currentOperator, parseInt(firstNumber), parseInt(secondNumber));
+        firstNumber = result;
+        currentOperator = e.target.id;
+        lastOperationScreen.textContent = `${result}` + ' ' + `${currentOperator}`;
+        console.log(`operator: ${currentOperator}`);
+        console.log(`result: ${result}`);
+        console.log(`current operator: ${currentOperator}`);
+        defaultZero();
+        } else {
+        firstNumber = currentOperationScreen.textContent;
+        currentOperator = e.target.id;
+        lastOperationScreen.textContent = firstNumber + ' ' + currentOperator;
+        defaultZero();  
+    }
+    console.log(`New secondNumber: ${secondNumber}`);
+
+      }
+
+function calculateFinalResult() {
+    secondNumber = currentOperationScreen.textContent;
+    let finalResult = operate(currentOperator, firstNumber, secondNumber);
+    currentOperationScreen.textContent = finalResult;
+    lastOperationScreen.textContent = parseInt(`${firstNumber}`) + ' ' + currentOperator + ' ' + parseInt(`${secondNumber}`);
+    // if (firstNumber === '') lastOperationScreen.textContent = 
+}
+
+function removeZero() {
+    currentOperationScreen.textContent = '';
+}
+
+function clearCalculation () {
+    currentOperationScreen.textContent = '0';
+    lastOperationScreen.textContent = '';
+    firstNumber = '';
+    secondNumber = '';
+}
+
+function defaultZero() {
+    currentOperationScreen.textContent = 0;
+} 
+
+let currentOperation = currentOperationScreen.textContent;
+
+function deleteLastNumber() {
+    if (currentOperation === '' || currentOperation === '0') return defaultZero();
+    currentOperationScreen.textContent = currentOperationScreen.textContent.slice(0, -1);
+    if (currentOperation === '') return currentOperationScreen.textContent = 0;
+    }
+
+// function deleteLastNumber() {
+//     if (currentOperationScreen.textContent === '' || currentOperationScreen.textContent === '0') return defaultZero();
+//     currentOperationScreen.textContent = currentOperationScreen.textContent.slice(0, -1);
+//     if (currentOperationScreen.textContent === '') return currentOperationScreen.textContent = 0;
+//     }
 
 function add (a,b) {
-    return a + b;
+return a + b;
 }
 
 function subtract (a,b) {
@@ -28,7 +111,7 @@ function operate (operator, a, b) {
         case '*':
             return multiply(a,b);
         case '÷':
-            if (b === 0) return "Error";
+            // if (b === 0) return "Error";
             return divide(a,b);
     }
 }
